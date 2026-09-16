052cd3873178721ed285ac04f4b6141e8891565a
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-09-16 20:21:01 +0100

    Get students from DB

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const pool = require('./database');
 const port = 3000;
 const app = express();
 
+app.get('/student', async (req, res) => {
+  const [rows] = await pool.query('SELECT * FROM students');
+  res.json(rows);
+})
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 })
