7f224bdc22ecb0f618661cc97f7f64a5389ff208
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-09-16 20:25:28 +0100

    Insert a new student into the DB

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const pool = require('./database');
 // TODO: .env file
 const port = 3000;
 const app = express();
+app.use(express.json())
 
 app.get('/student', async (req, res) => {
   const [rows] = await pool.query('SELECT * FROM students');
   res.json(rows);
+})
+
+app.post('/student', async (req, res) => {
+  const payload = req.body;
+  await pool.query('INSERT INTO students (name) VALUES (?)', payload.name)
+  res.send('OK')
 })
 
 app.listen(port, () => {
